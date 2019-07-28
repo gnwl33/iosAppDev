6c3db2b2a12b7d327d5172538dfc93f5543f8665
gnwl33 <gnwl33@gmail.com>
2019-07-28 17:17:29 -0400

    Initial

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Gene Lee on 6/28/19.
+//  Copyright © 2019 Gene Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
